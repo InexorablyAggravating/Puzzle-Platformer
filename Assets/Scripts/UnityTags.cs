986d1416996ad20f7aa2df0f0316c1802e470cca
Inexorably Aggravating <maedercj@gmail.com>
2021-01-28 17:24:24 -0600

    Add SpawnPoints

---
+++ Assets/Scripts/UnityTags.cs
@@ -0,0 +1,6 @@
+﻿public class UnityTags
+{
+        public const string COLLECTIBLE = "Collectible";
+        public const string SPAWN_POINT = "SpawnPoint";
+        public const string PLAYER = "Player";
+}
