9f55e6c4dc69278a872d615bbdb70da241533e15
Inexorably Aggravating <maedercj@gmail.com>
2021-01-29 22:36:48 -0600

    Add Jumper

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Jumper : MonoBehaviour
+    {
+        [SerializeField] private float jumpHeight;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag(UnityTags.PLAYER))
+            {
+                var con = other.GetComponent<PlayerController>();
+                con.velocity = new Vector2(con.velocity.x, jumpHeight);
+            }
+        }
+    }
+}
